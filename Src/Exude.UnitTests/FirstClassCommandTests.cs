d1bd7f44738a90bad1d84b42e943c12f810f72b7
Mark Seemann <mark@ploeh.dk>
2014-02-21 10:03:11 +0100

    Introduced FirstClassCommand,
    which will be the command that implements first-class, programmatic tests.
    The reason for adding this test now is mainly to have a single test, so
    that the rest of the setup can be completed, including build scripts, etc.
    
    However, the test is correct.

---
+++ Src/Exude.UnitTests/FirstClassCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Grean.Exude;
+using Xunit.Sdk;
+
+namespace Grean.Exude.UnitTests
+{
+    public class FirstClassCommandTests
+    {
+        [Fact]
+        public void SutIsTestCommand()
+        {
+            var sut = new FirstClassCommand();
+            Assert.IsAssignableFrom<ITestCommand>(sut);
+        }
+    }
+}
---
+++ Src/Exude/FirstClassCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit.Sdk;
+
+namespace Grean.Exude
+{
+    public class FirstClassCommand : TestCommand
+    {
+        public FirstClassCommand()
+            : base(Reflector.Wrap(((Action)(() => { })).Method), "", 0)
+        {
+        }
+
+        public override MethodResult Execute(object testClass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
