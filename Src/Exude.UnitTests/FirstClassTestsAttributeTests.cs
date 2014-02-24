216c27e8e4a9fcd9c4bd8d87feeda3d594cd9dc2
Mark Seemann <mark@ploeh.dk>
2014-02-24 08:55:06 +0100

    Returned FirstClassCommands from reflected method.

--- Src/Exude.UnitTests/FirstClassTestsAttributeTests.cs
+++ Src/Exude.UnitTests/FirstClassTestsAttributeTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using Grean.Exude;
+using Xunit.Sdk;
+using System.Reflection;
 
 namespace Grean.Exude.UnitTests
 {
@@ -14,6 +16,33 @@ namespace Grean.Exude.UnitTests
         {
             var sut = new FirstClassTestsAttribute();
             Assert.IsAssignableFrom<FactAttribute>(sut);
+        }
+
+        [Fact]
+        public void CreateTestMethodWithFirstClassTestsReturnsCorrectResult()
+        {
+            var method = Reflector.Wrap(this.GetType().GetMethod(
+                "CreateFirstClassTests",
+                BindingFlags.Static | BindingFlags.NonPublic));
+            var sut = new FirstClassTestsAttribute();
+
+            var actual = sut.CreateTestCommands(method);
+
+            Assert.Equal(
+                3,
+                actual.OfType<FirstClassCommand>().Count());
+            Assert.True(
+                actual
+                    .OfType<FirstClassCommand>()
+                    .All(fcc => fcc.TestAction.Method.DeclaringType == this.GetType()),
+                "All test actions should be declared here.");
+        }
+
+        private static IEnumerable<FirstClassCommand> CreateFirstClassTests()
+        {
+            yield return new FirstClassCommand(_ => { });
+            yield return new FirstClassCommand(_ => { });
+            yield return new FirstClassCommand(_ => { });
         }
     }
 }
--- Src/Exude/FirstClassTestsAttribute.cs
+++ Src/Exude/FirstClassTestsAttribute.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Grean.Exude
 {
     public class FirstClassTestsAttribute : FactAttribute
     {
+        protected override IEnumerable<ITestCommand> EnumerateTestCommands(
+            IMethodInfo method)
+        {
+            var testClassInstance = method.CreateInstance();
+            var returnValue = method.MethodInfo.Invoke(testClassInstance, null);
+            return from fcc in (IEnumerable<FirstClassCommand>)returnValue
+                   select fcc as ITestCommand;
+        }
     }
 }
