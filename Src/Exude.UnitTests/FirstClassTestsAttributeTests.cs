39a4a7499a0be7ff144145f929e1ada1a5b088cc
Mark Seemann <mark@ploeh.dk>
2014-02-24 08:13:50 +0100

    Introduced [FirstClassTests],
    which will be the test attribute that enables first-class programmatic
    tests.

---
+++ Src/Exude.UnitTests/FirstClassTestsAttributeTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Grean.Exude;
+
+namespace Grean.Exude.UnitTests
+{
+    public class FirstClassTestsAttributeTests
+    {
+        [Fact]
+        public void SutIsFactAttribute()
+        {
+            var sut = new FirstClassTestsAttribute();
+            Assert.IsAssignableFrom<FactAttribute>(sut);
+        }
+    }
+}
---
+++ Src/Exude/FirstClassTestsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Grean.Exude
+{
+    public class FirstClassTestsAttribute : FactAttribute
+    {
+    }
+}
