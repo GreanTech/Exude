bff06cfde0a00b1a08147f16710dcefe4d1a7023
Mark Seemann <mark@ploeh.dk>
2014-02-24 08:57:39 +0100

    Added an example of how to use [FirstClassTests].

---
+++ Src/Exude.UnitTests/Scenario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Grean.Exude.UnitTests
+{
+    public class Scenario
+    {
+        [FirstClassTests]
+        public static IEnumerable<FirstClassCommand> YieldFirstClassTests()
+        {
+            yield return new FirstClassCommand(_ => Assert.Equal(1, 1));
+            yield return new FirstClassCommand(_ => Assert.Equal(2, 2));
+            yield return new FirstClassCommand(_ => Assert.Equal(3, 3));
+        }
+    }
+}
