e3efb56310006ced93a4db9e95fb7368cfb1f2ce
Mark Seemann <mark@ploeh.dk>
2014-03-01 16:47:56 +0100

    Introduced TestCase and ITestCase,
    which will replace part of the role currently played by FirstClassCommand.
    The reason for this is that it turns out that the xUnit.net framework
    makes certain assumptions about the IMethodInfo instance passed to the
    TestCommand constructor. At the time a static test method starts
    enumerating test cases, there's not enough information available about the
    actual test method.
    
    Instead, the new approach will be to have test methods yield sequences of
    ITestCase, and the ITestCase interface will then subsequently have a
    conversion method that returns a proper ITestCommand.

---
+++ Src/Exude.UnitTests/TestCaseTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Grean.Exude;
+
+namespace Grean.Exude.UnitTests
+{
+    public class TestCaseTests
+    {
+        [Fact]
+        public void SutIsTestCase()
+        {
+            var sut = new TestCase();
+            Assert.IsAssignableFrom<ITestCase>(sut);
+        }
+    }
+}
