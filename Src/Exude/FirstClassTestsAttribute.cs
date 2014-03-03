c35117bb0024f7b8f74996f2d5bf331629c8bcd5
Mark Seemann <mark@ploeh.dk>
2014-03-03 22:35:11 +0100

    Documented FirstClassTestsAttribute.

--- Src/Exude/FirstClassTestsAttribute.cs
+++ Src/Exude/FirstClassTestsAttribute.cs
@@ -7,8 +7,67 @@ using Xunit.Sdk;
 
 namespace Grean.Exude
 {
+    /// <summary>
+    /// A test attribute used to adorn methods that creates first-class 
+    /// executable test cases.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The xUnit.net unit testing framework provides various ways in which you
+    /// can write Parameterized Tests, and while many of the options are
+    /// convenient, either they only support constants (like the [InlineData]
+    /// attribute), or they are releatively inconvenient to use, like the
+    /// [ClassData] attribute. In all cases of data attributes supporting the
+    /// [Theory] attribute, they are based on a contract of untyped (and boxed)
+    /// arrays.
+    /// </para>
+    /// <para>
+    /// The [FirstClassTests] attribute, on the other hand, enables you to
+    /// write executable test cases as strongly-typed, first-class test
+    /// objects.
+    /// </para>
+    /// <para>
+    /// Write a method that returns IEnumerable&lt;ITestCase&gt;, or a type
+    /// implementing that interface, like ITestCase[]; then adorn that method
+    /// with the [FirstClassTests] attribute. The test runner will then execute
+    /// all the returned test cases.
+    /// </para>
+    /// </remarks>
+    /// <example>
+    /// This simple example returns three test cases that all pass:
+    /// <code><![CDATA[[FirstClassTests]
+    /// public static IEnumerable<ITestCase> YieldFirstClassTests()
+    /// {
+    ///     yield return new TestCase(_ => Assert.Equal(1, 1));
+    ///     yield return new TestCase(_ => Assert.Equal(2, 2));
+    ///     yield return new TestCase(_ => Assert.Equal(3, 3));
+    /// }]]>
+    /// </code>
+    /// </example>
+    /// <seealso cref="ITestCase" />
     public class FirstClassTestsAttribute : FactAttribute
     {
+        /// <summary>
+        /// Enumerates the test commands represented by this test method, by
+        /// projecting the returned sequence of <see cref="ITestCase" /> into a
+        /// sequence of xUnit.net ITestCommand instances.
+        /// </summary>
+        /// <param name="method">
+        /// The test method adorned with the [FirstClassTests] attribute.
+        /// </param>
+        /// <returns>
+        /// The xUnit.net ITestCommand instances produced by converting this
+        /// method's returned sequence of <see cref="ITestCase" /> instances
+        /// into a sequence of ITestCommand instances.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="method" /> is <see langword="null" />.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// <paramref name="method" /> represents a method that doesn't return
+        /// IEnumerable&lt;ITestCase&gt; (or a compatible type, such as
+        /// ITestCase[]).
+        /// </exception>
         protected override IEnumerable<ITestCommand> EnumerateTestCommands(
             IMethodInfo method)
         {
