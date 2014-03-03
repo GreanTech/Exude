2b90bc58ef3550d9ca336dc075eb9a758a7c7be2
Mark Seemann <mark@ploeh.dk>
2014-03-03 22:17:53 +0100

    Added XML documentation to ITestCase.

--- Src/Exude/ITestCase.cs
+++ Src/Exude/ITestCase.cs
@@ -6,8 +6,24 @@ using Xunit.Sdk;
 
 namespace Grean.Exude
 {
+    /// <summary>
+    /// Represents a test-case that can be turned into an xUnit.net
+    /// ITestCommand when returned from a test method adorned with the
+    /// <see cref="FirstClassTestsAttribute" />.
+    /// </summary>
+    /// <seealso cref="FirstClassTestsAttribute" />
     public interface ITestCase
     {
+        /// <summary>
+        /// Converts the instance to an xUnit.net ITestCommand instance.
+        /// </summary>
+        /// <param name="method">
+        /// The method adorned by a <see cref="FirstClassTestsAttribute" />.
+        /// </param>
+        /// <returns>
+        /// An xUnit.net ITestCommand that represents the executable test case.
+        /// </returns>
+        /// <seealso cref="FirstClassTestsAttribute" />
         ITestCommand ConvertToTestCommand(IMethodInfo method);
     }
 }
